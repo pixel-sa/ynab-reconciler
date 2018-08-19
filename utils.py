74c5b641d6ee346e35c7aed15fd8b536f803da28
larryg727 <larrygonzales727@gmail.com>
2018-08-19 02:03:24 -0500

    Added funtionality to match up ynab transactions with bank transactions based on date and amount. Remaining unmatched transaction are put into separate list for further comparison

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import csv
 import json
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
 
 
@@ -14,6 +14,7 @@ def convert_csv_to_json(filename):
 
 def reconcile_differences(transactions, bank_transactions):
     matches = []
+
     for transaction in transactions:
         transaction_amount = transaction['amount'] / 1000
         transaction_payee = transaction['payee_name']
@@ -22,17 +23,67 @@ def reconcile_differences(transactions, bank_transactions):
         date_format = '%Y-%m-%d'
         transaction_date = datetime.strptime(transaction_date_str, date_format).date()
 
-        print(transaction_amount, transaction_date, transaction_payee)
+        minus_3_days = transaction_date - timedelta(days=3)
+        plus_3_days = transaction_date + timedelta(days=3)
+        print(minus_3_days, plus_3_days)
 
         for idx, bank_transaction in enumerate(bank_transactions):
             if idx == 0:
                 continue
 
-            bank_amount = bank_transaction['Debit'] if bank_transaction['Credit'] == '' else bank_transaction['Credit'] 
+            bank_amount = 0 - float(bank_transaction['Debit']) if bank_transaction['Credit'] == '' else float(bank_transaction['Credit'])
             bank_payee = bank_transaction['Description']
             
             bank_date_str = bank_transaction['Date']
             bank_date_list = bank_date_str.split('/')
             bank_date = datetime(int(bank_date_list[2]), int(bank_date_list[0]), int(bank_date_list[1])).date()
 
-            print(bank_amount, bank_date, bank_payee)
+            if bank_amount == transaction_amount and minus_3_days <= bank_date and bank_date <= plus_3_days:
+                previously_used = False
+                for check in matches:
+                    if check['bank'] == idx:
+                        previously_used = True
+                        print("Already used transaction")
+                        break
+                if not previously_used:
+                    new_match = {'bank': idx, 'ynab': transaction['id']}
+                    matches.append(new_match)
+                    print('MATCH!!')
+                    break
+
+    print(len(transactions), len(bank_transactions))
+    print(len(matches))
+
+    matches.sort(key=lambda x: x['bank'], reverse=False)
+    # print(matches)
+
+    unmatched_bank = []
+
+    for idx, bank_transaction in enumerate(bank_transactions):
+        match_found = False
+        for check in matches:
+            if check['bank'] == idx:
+                match_found = True
+                break
+
+        if not match_found:
+            unmatched_bank.append(bank_transaction)
+
+    print(len(unmatched_bank))
+    print(unmatched_bank)
+
+    unmatched_transactions = []
+
+    for transaction in transactions:
+        match_found = False
+        for check in matches:
+            if check['ynab'] == transaction['id']:
+                match_found = True
+                break
+
+        if not match_found:
+            unmatched_transactions.append(transaction)
+
+    print(len(unmatched_transactions))
+    print(unmatched_transactions)
+    
