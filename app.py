b74575bdff4170f978f9d007f83edeb242a1f4d9
Vivian <vcnales@gmail.com>
2018-08-18 20:44:21 -0500

    added logic to get transaction history

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import config 
+import csv
 import json
 import os
 import requests
+import time
+import pandas as pd
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from datetime import datetime, date, time, timedelta
+from difflib import SequenceMatcher
+
 
 app = Flask(__name__)
 
@@ -29,10 +35,39 @@ def get_budget():
 
     if response.status_code == 200:
         budget_list = response.json()
-        print(json.dumps(budget_list, indent=4))
+        # print(json.dumps(budget_list, indent=4))
         return render_template("about.html", budgets=budget_list)
     else:
         return("Error!!! " + str(response.status_code))
+
+@app.route('/api/transactions', methods=['GET'])
+def get_transactions():
+    print("transaction api!!!")
+    budget_id = request.args['budgetId']
+
+    access_token = config.access_token
+    header = {'Authorization': 'Bearer ' + access_token}
+
+    get_accounts_url = f'https://api.youneedabudget.com/v1/budgets/{budget_id}/accounts'
+    accounts_response = requests.get(get_accounts_url, headers=header)
+    # print(accounts_response.json())
+
+    query = '?since_date=2018-08-01'
+    url = f'https://api.youneedabudget.com/v1/budgets/{budget_id}/transactions' + query
+    response = requests.get(url, headers=header)
+
+    print(response)
+    print(response.headers)
+    # print(response.json())
+    transaction_list = response.json()
+    # print(json.dumps(transaction_list, indent=4))
+    print(type(transaction_list))
+    print(len(transaction_list['data']['transactions']))
+
+
+
+    return jsonify("yay!")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
