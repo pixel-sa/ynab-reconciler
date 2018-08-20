f1d9dfccaa80d7f0ca60f298d9cb61826897d4dc
larryg727 <larrygonzales727@gmail.com>
2018-08-19 19:58:53 -0500

    Connected and reformatted reconciler to take data from csv uploader

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import requests
 from datetime import date, datetime, timedelta
 
 
@@ -13,13 +14,15 @@ def convert_csv_to_json(filename):
     data = json.dumps([r for r in csv_reader])
     return json.loads(data)
 
-def reconcile_differences(transactions, bank_transactions, since_date):
+def reconcile_differences(transactions, converted_bank_csv, since_date):
     matches = []
-
+    bank_transactions = converted_bank_csv['transactions']
+    
     for transaction in transactions:
+    
         transaction_amount = transaction['amount'] / 1000
         transaction_payee = transaction['payee_name']
-
+        print(transaction_payee + ' - ' + str(transaction_amount))
         transaction_date_str = transaction['date']
         date_format = '%Y-%m-%d'
         transaction_date = datetime.strptime(transaction_date_str, date_format).date()
@@ -28,9 +31,6 @@ def reconcile_differences(transactions, bank_transactions, since_date):
         plus_3_days = transaction_date + timedelta(days=3)
 
         for idx, bank_transaction in enumerate(bank_transactions):
-            if idx == 0:
-                continue
-
             bank_amount = 0 - float(bank_transaction['Debit']) if bank_transaction['Credit'] == '' else float(bank_transaction['Credit'])
             bank_payee = bank_transaction['Description']
             
@@ -63,8 +63,6 @@ def reconcile_differences(transactions, bank_transactions, since_date):
 
     for idx, bank_transaction in enumerate(bank_transactions):
         match_found = False
-        if idx == 0:
-            continue
 
         bank_date_str = bank_transaction['Date']
         bank_date_list = bank_date_str.split('/')
@@ -102,3 +100,20 @@ def reconcile_differences(transactions, bank_transactions, since_date):
     print(len(unmatched_transactions))
     print(unmatched_transactions)
     print('-----------------------------------------')
+    
+    return {'bank': unmatched_bank, 'ynab': unmatched_transactions}
+
+
+def get_ynab_transactions(access_token, budget_id, account_id):
+    header = {'Authorization': 'Bearer ' + access_token}
+    since_date = datetime.now().date() - timedelta(days=10)
+    
+    query = f'?since_date={since_date}'
+    url = f'https://api.youneedabudget.com/v1/budgets/{budget_id}/accounts/{account_id}/transactions' + query
+    response = requests.get(url, headers=header)
+    
+    data = json.loads(response.content.decode('utf-8'))
+    transaction_list = data['data']['transactions']
+    
+
+    return transaction_list
